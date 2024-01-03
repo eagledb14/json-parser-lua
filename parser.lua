d5f76b3714f30fb460773d63fc5c33a3a2021250
eagledb14 <eagledb14@gmail.com>
2024-01-02 20:08:33 -0500

    added function to convert lua tables to json

--- parser.lua
+++ parser.lua
@@ -105,7 +105,52 @@ function parser:parse(input)
   return json
 end
 
+local function is_array(tbl)
+  for i, _ in pairs(tbl) do
+    if type(i) ~= "number" then
+      return false
+    end
+  end
+  return true
+end
+
 function parser.to_json(input)
+  local function escapeString(s)
+        s = string.gsub(s, "\\", "\\\\")
+        s = string.gsub(s, "\"", "\\\"")
+        s = string.gsub(s, "\n", "\\n")
+        s = string.gsub(s, "\r", "\\r")
+        s = string.gsub(s, "\t", "\\t")
+        return s
+    end
+
+    local function serialize(val)
+        if type(val) == "table" then
+            local isListFlag = is_array(val)
+            local result = {}
+            for k, v in pairs(val) do
+                if isListFlag then
+                    table.insert(result, serialize(v))
+                else
+                    local key = type(k) == "string" and '"' .. escapeString(k) .. '"' or serialize(k)
+                    table.insert(result, key .. ":" .. serialize(v))
+                end
+            end
+            if isListFlag then
+                return "[" .. table.concat(result, ",") .. "]"
+            else
+                return "{" .. table.concat(result, ",") .. "}"
+            end
+        elseif type(val) == "number" or type(val) == "boolean" then
+            return tostring(val)
+        elseif type(val) == "string" then
+            return '"' .. escapeString(val) .. '"'
+        else
+            return '"Unsupported Type"'
+        end
+    end
+
+    return serialize(input)
 end
 
 return parser
